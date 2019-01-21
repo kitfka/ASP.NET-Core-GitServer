2472853a31cfb69560f27102d08ae468fd2bb082
InfDev <alex.shlyakhto@gmail.com>
2019-01-21 13:08:26 +0200

    Change Views to english

---
+++ GitServer/Constants/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitServer
+{
+    public class Constants
+    {
+        public const string DefaultCulture = "en";
+    }
+}
--- GitServer/Startup.cs
+++ GitServer/Startup.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Claims;
+using System.Globalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using GitServer.Infrastructure;
 using GitServer.Services;
@@ -78,6 +83,19 @@ namespace GitServer
 			services.AddTransient<GitFileService>();
             services.AddTransient<IRepository<User>, Repository<User>>();
             services.AddTransient<IRepository<Repository>, Repository<Repository>>();
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                     new CultureInfo("en"),
+                     new CultureInfo("ru"),
+                     new CultureInfo("uk")
+                };
+                options.DefaultRequestCulture = new RequestCulture(culture: Constants.DefaultCulture, uiCulture: Constants.DefaultCulture);
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -92,6 +110,11 @@ namespace GitServer
 			{
 				app.UseExceptionHandler("/error");
 			}
+
+            //https://docs.microsoft.com/en-us/aspnet/core/fundamentals/localization
+            //https://msdn.microsoft.com/en-us/library/ee825488(v=cs.20).aspx
+            var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
 
             app.UseAuthentication();
             app.UseStaticFiles();
