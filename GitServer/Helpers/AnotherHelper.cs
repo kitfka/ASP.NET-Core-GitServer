6c9ce5329bcf9dad234b6c7c25e6f175ad5b22db
Kitfka <philipthuys@gmail.com>
2023-06-20 20:13:15 +0200

    move rest to .net6 and first attempt to fix commit message in tree

---
+++ GitServer/Helpers/AnotherHelper.cs
@@ -0,0 +1,30 @@
+﻿using GitServer.ApplicationCore.Models;
+using GitServer.Extensions;
+using GitServer.Models;
+using LibGit2Sharp;
+
+namespace GitServer.Helpers;
+
+public static class AnotherHelper
+{
+    public static string Spike(LibGit2Sharp.Repository repo, FileViewModel fvm)
+    {
+
+        //repo.
+        //var result = repo.Lookup<Commit>(fvm.Object.Sha);
+        //var result = repo.Lookup<Commit>(fvm.Object.Sha);
+
+        string result = "";
+
+        foreach (var commit in repo.Commits)
+        {
+            if (commit.Id == fvm.Object.Id)
+            {
+                result = commit.Message;
+            }
+        }
+
+
+        return result;
+    }
+}
--- GitServer/Program.cs
+++ GitServer/Program.cs
@@ -7,20 +7,19 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace GitServer
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+namespace GitServer;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
